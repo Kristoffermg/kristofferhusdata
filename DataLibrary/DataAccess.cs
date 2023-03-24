fd86bf5b7a702f388c91cd6cf7292ee41696cc31
Kristoffermg <kristoffermoeller151@gmail.com>
2023-03-24 11:46:05 +0100

    DataLibrary changes
    -> made a folder for DataLibrary in the main scope

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kristofferhusdata.DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string query, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(query, parameters);
+
+                return rows.ToList();
+            }
+        }
+    }
+}
---
+++ DataLibrary/IDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace kristofferhusdata.DataLibrary
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string query, U parameters, string connectionString);
+    }
+}
